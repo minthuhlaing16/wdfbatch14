c699b066cf1d01d3fc6f47a06aad1868f0882696
minthuhlaing16 <minthuhlaing786@gmail.com>
2025-10-11 20:34:31 +0630

    document object modal

---
+++ js/documentobjectmodal.js
@@ -0,0 +1,13 @@
+// DOM (Document Object Modal), programming interface
+// to access Web Page (HTML Document)
+// (i) Search;
+// (ii) create new Element;
+// (iii) edit ;
+// (iv) remove;
+// (v) action to Events;
+// __________________________________________________________________________________________________________
+// getElementById() = ID (element direct hwat tal)
+// getElementsByClassName() = Class
+// getElementByTagName() = Tag
+// querySelector() = ID,Class,Tag (element direct htwat tal)
+// querySelectorAll() ID,Class,Tag (Array nae lar mar)
