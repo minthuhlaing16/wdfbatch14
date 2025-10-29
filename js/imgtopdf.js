32533039fd9290c1851df64c55cb8c05ac0403dd
minthuhlaing16 <minthuhlaing786@gmail.com>
2025-10-29 16:41:27 +0630

    finish js pj

--- js/imgtopdf.js
+++ js/imgtopdf.js
@@ -41,10 +41,51 @@ getfileinput.addEventListener("change", function (e) {
 });
 
 function pdfloader() {
+  // todo 1. Check if image is exists or not
   if (!newimg) {
     alert("Please upload img first!");
     // ! out ka code tway a lote ma lote say chin loa return pyan htr tar
     return;
   }
-  console.log("pdf is working...");
+  // console.log("pdf is working...");
+
+  // todo 2. Import package and instance
+  // ! import lote tat nii
+  const { jsPDF } = window.jspdf;
+
+  // ? Default export is a4 paper, portrait using millimeters for units
+  const doc = new jsPDF({
+    orientation: "landscape", //? portrait
+    unit: "mm",
+  });
+
+  // todo 3. Create canvas as image actual dimension
+  const canvas = document.createElement("canvas");
+  canvas.width = newimg.naturalWidth;
+  canvas.height = newimg.height;
+  console.log(canvas);
+
+  // todo 4. Draw image on canvas
+  // ? drawing contex
+  const ctx = canvas.getContext("2d");
+  ctx.drawImage(newimg, 0, 0); // ? parameter 5 khu hti pay loa ya tal => (img,dx,dy) ==> dx = destination x ,dy = destination y;
+  console.log(ctx); // ? CanvasRenderingContext2D
+
+  // todo 5. Convert canvas to jpeg(Quality 0 to 1)
+  const imgdata = canvas.toDataURL("image/jpeg", 1); //? 3 khu pay loa ya tal ==> image/jpeg, image/webp, image/png
+
+  // todo 6. Download img to pdf => parameter = (image,x-coordinate,y-coordinate,width,height)
+  doc.addImage(imgdata, 10, 10, 200, 80);
+  doc.save("newimagefile.pdf");
 }
+
+// console.log(window.jspdf);
+// console.log(window.jspdf.jsPDF);
+
+// todo Method 1
+// let jspdfObj = window.jspdf.jsPDF;
+// console.log(jspdfObj);
+
+// todo Method 2 ==> Object destructuring
+// let { jsPDF } = window.jspdf;
+// console.log(jsPDF);
